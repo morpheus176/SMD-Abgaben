ef92a9f43bed69b8c20ac435b134a68fca289402
Lars <lars.kolk@tu-dortmund.de>
2018-11-07 21:35:49 +0100

    Code verkürzt

--- Blatt 03/Aufgabe08/Aufgabe08.py
+++ Blatt 03/Aufgabe08/Aufgabe08.py
@@ -10,54 +10,35 @@ np.random.seed(42)
 
 N=15/(np.pi)**4
 
-
-# a)
-
 def f(x):
     try:
         return N*x**3/(np.e**x-1)
     except ZeroDivisionError:
         return 0
 
-max_x = optimize.fmin(lambda x: -f(x), 0)
+max_x = optimize.fmin(lambda x: -f(x), 0, disp=False)
 
-xin=[]
-yin=[]
-xout=[]
-yout=[]
+#gibt den maximalwert der Funktion f(x) aus. Als Funktion damit Rejection damit arbeiten kann.
+def fmax(xx):
+    return f(max_x)
 
-start_time = timeit.default_timer()
+def rejection(funktion, Start, Ende):
+    xin=[]
+    yin=[]
+    xout=[]
+    yout=[]
 
-while len(xin)<10**5:
-    xx=np.random.uniform(0, 20)
-    yy=np.random.uniform(0, f(max_x))
-    if yy <= f(xx):
-        xin.append(xx)
-        yin.append(yy)
-    else:
-        xout.append(xx)
-        yout.append(yy)
+    while len(xin)<10**5:
+        xx=np.random.uniform(Start, Ende)
+        yy=np.random.uniform(Start, funktion(xx))
+        if yy <= f(xx):
+            xin.append(xx)
+            yin.append(yy)
+        else:
+            xout.append(xx)
+            yout.append(yy)
 
-
-laufzeit_a = timeit.default_timer() - start_time
-
-x=np.linspace(0, 20, 500)
-plt.plot(xin, yin, 'b.', markersize=0.05)# alpha=0.01)
-plt.plot(xout, yout, 'r.', markersize=0.05)# alpha=0.01)
-plt.plot(x, f(x), 'k-', label=r'Planck-Verteilung')
-#plt.hlines(y=f(max_x), xmin=0, xmax=20, linewidth=1, color='black')
-#plt.vlines(x=max(x), ymin=0, ymax=f(max_x), linewidth=1, color='black')
-#plt.vlines(x=max(-x), ymin=0, ymax=f(max_x), linewidth=1, color='black')
-plt.legend(loc='upper right')
-plt.tight_layout()
-plt.savefig("a.pdf")
-
-plt.clf()
-
-verworfen_a = len(xout)
-
-
-# b)
+    return xin, yin, xout, yout
 
 def g(x):
     return 200*N*x**(-0.1)*np.exp(-x**0.9)
@@ -67,32 +48,36 @@ def h(x):
 
 x_s = brentq(h, 5, 10)
 
-xin=[]
-yin=[]
-xout=[]
-yout=[]
+def gg(x):
+    if x < x_s:
+        return f(max_x)
+    else:
+        return 200*N*x**(-0.1)*np.exp(-x**0.9)
+
+# a)
 
 start_time = timeit.default_timer()
 
-while len(xin)<10**5:
-    xx=np.random.uniform(0, 15)
-    if xx <= x_s:
-        yy=np.random.uniform(0, f(max_x))
-        if yy <= f(xx):
-            xin.append(xx)
-            yin.append(yy)
-        else:
-            xout.append(xx)
-            yout.append(yy)
-    else:
-        yy=np.random.uniform(0, g(xx))
-        if yy <= f(xx):
-            xin.append(xx)
-            yin.append(yy)
-        else:
-            xout.append(xx)
-            yout.append(yy)
+xin, yin, xout, yout = rejection(fmax, 0, 20)
 
+laufzeit_a = timeit.default_timer() - start_time
+
+x=np.linspace(0, 20, 500)
+plt.plot(xin, yin, 'b.', markersize=0.05)# alpha=0.01)
+plt.plot(xout, yout, 'r.', markersize=0.05)# alpha=0.01)
+plt.plot(x, f(x), 'k-', label=r'Planck-Verteilung')
+plt.legend(loc='upper right')
+plt.tight_layout()
+plt.savefig("a.pdf")
+plt.clf()
+
+verworfen_a = len(xout)
+
+
+# b)
+start_time = timeit.default_timer()
+
+xin, yin, xout, yout = rejection(gg, 0, 15)
 
 laufzeit_b = timeit.default_timer() - start_time
 
@@ -113,4 +98,4 @@ plt.savefig('b.pdf')
 
 print('\n Anzahl der verworfenen Ereignisse: \n a) %0.0f \n b) %0.0f' % (verworfen_a, verworfen_b))
 print('\n Laufzeit: \n a) %0.2f s \n b) %0.2f s' % (laufzeit_a, laufzeit_b))
-print('\n Verhältnisse a)/b): \n verworfene Ereignisse: %0.2f \n Laufzeit: %0.2f' % (verworfen_a/verworfen_b, laufzeit_a/laufzeit_b))
+print('\n Verhaeltnisse a)/b): \n verworfene Ereignisse: %0.2f \n Laufzeit: %0.2f' % (verworfen_a/verworfen_b, laufzeit_a/laufzeit_b))
